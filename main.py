c52de7eb21d859e9eb32badff0caed295d8cb147
John Martin Infante <john.infante@ripeconcepts.net>
2025-09-03 10:08:43 -0700

    Test Webhook

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import os
 from dotenv import load_dotenv
-import httpx #change 3
+import httpx #change 3 # change 4 is on the .env file
 
 load_dotenv()
 
